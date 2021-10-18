e8fbaa514c7a0b5d797d6c7af84a6af47bfcc6ec
jmichaelss <njoelmichael1999@gmail.com>
2021-10-18 13:32:35 +0100

    Show User Location Implementation

--- App.js
+++ App.js
@@ -1,41 +1,69 @@
-import React from "react";
-// import { useState, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; 
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {useEffect} from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {StyleSheet, Text, View} from 'react-native';
 
+const App = () => {
+  useEffect(() => {
+    const handleLocationPermission = async () => {
+      let checkIfGranted = '';
+      if (Platform.OS === 'ios') {
+        checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
 
+        if (
+          checkIfGranted === RESULTS.BLOCKED ||
+          checkIfGranted === RESULTS.DENIED
+        ) {
+          const makeRequest = await request(
+            PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+          );
+        }
+      }
+      if (Platform.OS === 'android') {
+        checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
 
-
-
-  export default () => (
+        if (
+          checkIfGranted === RESULTS.BLOCKED ||
+          checkIfGranted === RESULTS.DENIED
+        ) {
+          const makeRequest = await request(
+            PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+          );
+        }
+      }
+    };
+    handleLocationPermission();
+  }, []);
+  return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={{
+        initialRegion={{
           latitude: 50.8677,
-         longitude: 0.0875,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
+          longitude: 0.0875,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
         }}
-      >
-      </MapView>
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+      />
     </View>
- );
-
-
+  );
+};
 
 const styles = StyleSheet.create({
- container: {
-   ...StyleSheet.absoluteFillObject,
-   flex:1,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
-   flex: 1,
-   
-   
- },
- map: {
-   ...StyleSheet.absoluteFillObject,
- },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
+
+export default App;
