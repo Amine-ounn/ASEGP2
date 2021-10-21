fe6a1de663ee6cba79a89d97e8541a415aee4db0
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2021-10-21 10:51:56 +0400

    feat: get current location

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {useEffect} from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
-import {StyleSheet, Platform, View, Text} from 'react-native';
+import {StyleSheet, Platform, View, Text, Alert} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
+const LATITUDE = 50.8677;
+const LONGITUDE = 50.8677;
+const LATITUDE_DELTA = 0.015;
+const LONGITUDE_DELTA = 0.0121;
 
 const App = () => {
   const [location, setLocation] = React.useState({
-    latitude: 50.8677,
-    longitude: 0.0875,
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
   });
 
   useEffect(() => {
     const handleLocationPermission = async () => {
       let checkIfGranted = '';
 
+      // check if location permission is granted
+      // if not, request for permission
+
+      // iOS
       if (Platform.OS === 'ios') {
         checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
 
@@ -24,8 +32,12 @@ const App = () => {
           checkIfGranted === RESULTS.DENIED
         ) {
           await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+        } else {
+          getCurrentLocation();
         }
       }
+
+      // Android
       if (Platform.OS === 'android') {
         checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
 
@@ -34,6 +46,8 @@ const App = () => {
           checkIfGranted === RESULTS.DENIED
         ) {
           await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+        } else {
+          getCurrentLocation();
         }
       }
     };
@@ -41,15 +55,39 @@ const App = () => {
     handleLocationPermission();
   }, []);
 
+  const getCurrentLocation = () => {
+    // will give you the current location
+    Geolocation.getCurrentPosition(
+      position =>
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }),
+      error => {
+        Alert.alert(error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 5000},
+    );
+  };
+
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        initialRegion={location}
+        region={{
+          ...location,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
         showsUserLocation={true}
-        showsMyLocationButton={true}
-      />
+        showsMyLocationButton={true}>
+        <Marker
+          coordinate={location}
+          title="You"
+          description="This is your current location"
+        />
+      </MapView>
 
       <View style={styles.modalContainer}>
         <View style={styles.modal}>
