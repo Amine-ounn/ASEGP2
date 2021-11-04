142e37441a78c3e0778b0b912b7caa26bc4f3fd9
Olushola Temiloluwa <johntemiloluwa.o@gmail.com>
2021-11-04 21:15:11 +0000

    feat: send location to server

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import {StyleSheet, Platform, View, Text, Alert} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
-import axios from ‘axios’;
+import axios from 'axios';
 
 const LATITUDE = 50.8677;
 const LONGITUDE = 50.8677;
@@ -18,67 +18,74 @@ const App = () => {
   });
 
   useEffect(() => {
-    const handleLocationPermission = async () => {
-      let checkIfGranted = '';
-
-      // check if location permission is granted
-      // if not, request for permission
-
-      // iOS
-      if (Platform.OS === 'ios') {
-        checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
-        if (
-          checkIfGranted === RESULTS.BLOCKED ||
-          checkIfGranted === RESULTS.DENIED
-        ) {
-          await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-        } else {
-          getCurrentLocation();
-        }
-      }
-
-      // Android
-      if (Platform.OS === 'android') {
-        checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
-        if (
-          checkIfGranted === RESULTS.BLOCKED ||
-          checkIfGranted === RESULTS.DENIED
-        ) {
-          await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-        } else {
-          getCurrentLocation();
-        }
-      }
-    };
-
     handleLocationPermission();
-    getCurrentLocation();
-    trackUserLocation();
+
+    const watchId = trackUserLocation();
+
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
   });
 
-  const getCurrentLocation = () => {
-    // will give you the current location
-    const id = Geolocation.getCurrentPosition(
-      position => {
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      error => {
-        Alert.alert(error.message);
-      },
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 5000},
-    );
+  useEffect(() => {
+    // send location to server
+    const sendLocation = async () => {
+      const {latitude, longitude} = location;
 
-    return id;
+      axios
+        .post('https://ase2task3.herokuapp.com/api/create_locations/', {
+          lat: latitude,
+          lng: longitude,
+          MAC: '123dslkd23',
+        })
+        .then(res => {})
+        .catch(err => {
+          console.log(err);
+          Alert.alert('Ooops', 'Something went while trying to save location', [
+            {text: 'Try again', onPress: () => sendLocation()},
+            {text: 'Cancel'},
+          ]);
+        });
+    };
+    console.log('location changed', location);
+
+    sendLocation();
+  }, [location]);
+
+  const handleLocationPermission = async () => {
+    let checkIfGranted = '';
+
+    // check if location permission is granted
+    // if not, request for permission
+
+    // iOS
+    if (Platform.OS === 'ios') {
+      checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+      if (
+        checkIfGranted === RESULTS.BLOCKED ||
+        checkIfGranted === RESULTS.DENIED
+      ) {
+        await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      }
+    }
+
+    // Android
+    if (Platform.OS === 'android') {
+      checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+      if (
+        checkIfGranted === RESULTS.BLOCKED ||
+        checkIfGranted === RESULTS.DENIED
+      ) {
+        await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      }
+    }
   };
 
   const trackUserLocation = () => {
     // will track the user location
-    Geolocation.watchPosition(
+    const watchId = Geolocation.watchPosition(
       position =>
         setLocation({
           latitude: position.coords.latitude,
@@ -87,28 +94,12 @@ const App = () => {
       error => {
         Alert.alert(error.message);
       },
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 5000},
+      {enableHighAccuracy: true, useSignificantChanges: true},
     );
+
+    return watchId;
   };
-  
- const params = JSON.stringify({
 
-"lat": position.coords.latitude,
-"lng": position.coords.longitude,
-"MAC": 'XD3254DQ',   
-
-});
-  
- // Send a POST request
-axios.post("https://ase2task3.herokuapp.com/api/create_locations/", params,{
-
-"headers": {
-
-"content-type": "application/json",
-
-},
-
-}); 
   return (
     <View style={styles.container}>
       <MapView
@@ -119,8 +110,7 @@ axios.post("https://ase2task3.herokuapp.com/api/create_locations/", params,{
           latitudeDelta: LATITUDE_DELTA,
           longitudeDelta: LONGITUDE_DELTA,
         }}
-        showsUserLocation={true}
-        showsMyLocationButton={true}>
+        showsUserLocation={true}>
         <Marker
           coordinate={location}
           title="You"
