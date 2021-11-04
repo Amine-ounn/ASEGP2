72934ec93f1fdefb6b8bc7c5d58575b709fcbf27
Amine-ounn <92341438+Amine-ounn@users.noreply.github.com>
2021-11-04 18:30:12 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import {StyleSheet, Platform, View, Text, Alert} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import axios from ‘axios’;
 
 const LATITUDE = 50.8677;
 const LONGITUDE = 50.8677;
@@ -89,7 +90,24 @@ const App = () => {
       {enableHighAccuracy: true, timeout: 15000, maximumAge: 5000},
     );
   };
+  
+ const params = JSON.stringify({
 
+"latitude": latitude,
+"longitude": longitude,
+
+});
+  
+ // Send a POST request
+axios.post(url, params,{
+
+"headers": {
+
+"content-type": "application/json",
+
+},
+
+}); 
   return (
     <View style={styles.container}>
       <MapView
