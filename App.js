6bb396d5247a74170561da5f14552927e3042bb5
unknown <aminehazard@gmail.com>
2021-11-23 13:57:48 +0000

    chore: update files

--- App.js
+++ App.js
@@ -5,11 +5,7 @@ import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 import { StyleSheet, Platform, View, Text, Alert } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import axios from 'axios';
-<<<<<<< HEAD
-import {getUniqueId} from 'react-native-device-info';
-=======
 import { getUniqueId } from 'react-native-device-info';
->>>>>>> master
 import moment from 'moment';
 
 const LATITUDE = 50.8677;
@@ -18,249 +14,206 @@ const LATITUDE_DELTA = 0.015;
 const LONGITUDE_DELTA = 0.0121;
 
 const App = () => {
-  const [location, setLocation] = useState({
-    latitude: LATITUDE,
-    longitude: LONGITUDE,
-  });
+    const [location, setLocation] = useState({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+    });
 
-  const [lastUpdate, setLastUpdate] = useState(null);
+    const [lastUpdate, setLastUpdate] = useState(null);
 
-  useEffect(() => {
-    try {
-      handleLocationPermission();
-<<<<<<< HEAD
-      getCurrentLocation();
-      const watchId = trackUserLocation();
+    useEffect(() => {
+        try {
+            handleLocationPermission();
+            // getCurrentLocation();
 
-      return () => {
-        Geolocation.clearWatch(watchId);
-      };
-    } catch (e) {
-      Alert.alert('Ooops', 'Something went wrong');
-    }
-  });
+            const watchId = trackUserLocation();
 
-=======
-      // getCurrentLocation();
+            return () => {
+                Geolocation.clearWatch(watchId);
+            };
+        } catch (e) {
+            Alert.alert('Ooops', 'Something went wrong');
+        }
+    }, []);
 
-      const watchId = trackUserLocation();
+    useEffect(() => {
+        const deviceId = getUniqueId();
 
-      return () => {
-        Geolocation.clearWatch(watchId);
-      };
-    } catch (e) {
-      Alert.alert('Ooops', 'Something went wrong');
-    }
-  }, []);
+        // send location to server
+        const sendLocation = async () => {
+            const { latitude, longitude } = location;
 
->>>>>>> master
-  useEffect(() => {
-    const deviceId = getUniqueId();
+            axios
+                .post('https://ase2task3.herokuapp.com/api/create_locations/', {
+                    lat: latitude,
+                    lng: longitude,
+                    MAC: deviceId,
+                })
+                .then(res => {
+                    setLastUpdate(res.data.time);
+                })
+                .catch(err => {
+                    console.log(err);
+                    Alert.alert(
+                        'Ooops',
+                        'Something went wrong while trying to save location',
+                        [
+                            { text: 'Try again', onPress: () => sendLocation() },
+                            { text: 'Cancel' },
+                        ],
+                    );
+                });
+        };
 
-    // send location to server
-    const sendLocation = async () => {
-<<<<<<< HEAD
-      const {latitude, longitude} = location;
-=======
-      const { latitude, longitude } = location;
->>>>>>> master
+        sendLocation();
+    }, [location]);
 
-      axios
-        .post('https://ase2task3.herokuapp.com/api/create_locations/', {
-          lat: latitude,
-          lng: longitude,
-          MAC: deviceId,
-        })
-        .then(res => {
-          setLastUpdate(res.data.time);
-        })
-        .catch(err => {
-          console.log(err);
-          Alert.alert(
-            'Ooops',
-            'Something went wrong while trying to save location',
-            [
-<<<<<<< HEAD
-              {text: 'Try again', onPress: () => sendLocation()},
-              {text: 'Cancel'},
-=======
-              { text: 'Try again', onPress: () => sendLocation() },
-              { text: 'Cancel' },
->>>>>>> master
-            ],
-          );
-        });
+    const handleLocationPermission = async () => {
+        let checkIfGranted = '';
+
+        // check if location permission is granted
+        // if not, request for permission
+
+        // iOS
+        if (Platform.OS === 'ios') {
+            checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+            if (
+                checkIfGranted === RESULTS.BLOCKED ||
+                checkIfGranted === RESULTS.DENIED
+            ) {
+                await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            }
+        }
+
+        // Android
+        if (Platform.OS === 'android') {
+            checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+            if (
+                checkIfGranted === RESULTS.BLOCKED ||
+                checkIfGranted === RESULTS.DENIED
+            ) {
+                await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+            }
+        }
     };
 
-    sendLocation();
-  }, [location]);
+    const trackUserLocation = () => {
+        // will track the user location
+        const watchId = Geolocation.watchPosition(
+            position =>
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                }),
+            error => {
+                Alert.alert(error.message);
+            },
+            { enableHighAccuracy: true, useSignificantChanges: true },
+        );
 
-  const handleLocationPermission = async () => {
-    let checkIfGranted = '';
+        return watchId;
+    };
 
-    // check if location permission is granted
-    // if not, request for permission
+    const getCurrentLocation = () => {
+        return Geolocation.getCurrentPosition(
+            position =>
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                }),
+            error => {
+                Alert.alert(error.message);
+            },
+            { enableHighAccuracy: true, useSignificantChanges: true },
+        );
+    };
 
-    // iOS
-    if (Platform.OS === 'ios') {
-      checkIfGranted = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    return (
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={{
+                    ...location,
+                    latitudeDelta: LATITUDE_DELTA,
+                    longitudeDelta: LONGITUDE_DELTA,
+                }}
+                showsUserLocation={true}
+                showsMyLocationButton={true}>
+                <Marker
+                    coordinate={location}
+                    title="You"
+                    description="This is your current location"
+                />
+            </MapView>
 
-      if (
-        checkIfGranted === RESULTS.BLOCKED ||
-        checkIfGranted === RESULTS.DENIED
-      ) {
-        await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-      }
-    }
-
-    // Android
-    if (Platform.OS === 'android') {
-      checkIfGranted = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
-      if (
-        checkIfGranted === RESULTS.BLOCKED ||
-        checkIfGranted === RESULTS.DENIED
-      ) {
-        await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-      }
-    }
-  };
-
-  const trackUserLocation = () => {
-    // will track the user location
-    const watchId = Geolocation.watchPosition(
-      position =>
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        }),
-      error => {
-        Alert.alert(error.message);
-      },
-<<<<<<< HEAD
-      {enableHighAccuracy: true, useSignificantChanges: true},
-=======
-      { enableHighAccuracy: true, useSignificantChanges: true },
->>>>>>> master
-    );
-
-    return watchId;
-  };
-
-  const getCurrentLocation = () => {
-<<<<<<< HEAD
-    Geolocation.getCurrentPosition(
-=======
-    return Geolocation.getCurrentPosition(
->>>>>>> master
-      position =>
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        }),
-      error => {
-        Alert.alert(error.message);
-      },
-<<<<<<< HEAD
-      {enableHighAccuracy: true, useSignificantChanges: true},
-=======
-      { enableHighAccuracy: true, useSignificantChanges: true },
->>>>>>> master
-    );
-  };
-
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          ...location,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        }}
-        showsUserLocation={true}
-        showsMyLocationButton={true}>
-        <Marker
-          coordinate={location}
-          title="You"
-          description="This is your current location"
-        />
-      </MapView>
-
-      <View style={styles.modalContainer}>
-        <View style={styles.modal}>
-          <View>
-            <Text style={styles.title}>Coordinates</Text>
-            <Text style={styles.coordinates}>
-              {location.latitude}, {location.longitude}
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.lastUpdateLabel}>Last Saved</Text>
-            <Text style={styles.lastUpdate}>
-              {moment(lastUpdate).format('h:mm:ss A')}
-            </Text>
-          </View>
+            <View style={styles.modalContainer}>
+                <View style={styles.modal}>
+                    <View>
+                        <Text style={styles.title}>Coordinates</Text>
+                        <Text style={styles.coordinates}>
+                            {location.latitude}, {location.longitude}
+                        </Text>
+                    </View>
+                    <View>
+                        <Text style={styles.lastUpdateLabel}>Last Saved</Text>
+                        <Text style={styles.lastUpdate}>
+                            {moment(lastUpdate).format('h:mm:ss A')}
+                        </Text>
+                    </View>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-    height: '88%',
-  },
-  modalContainer: {
-    flex: 1,
-    position: 'absolute',
-    height: '12%',
-    width: '100%',
-    minHeight: 0,
-    backgroundColor: '#fff',
-    zIndex: 1,
-    padding: 20,
-  },
-  modal: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'row',
-<<<<<<< HEAD
-=======
-    alignItems: 'center',
->>>>>>> master
-    width: '100%',
-    justifyContent: 'space-between',
-  },
-  title: {
-    fontSize: 20,
-    color: 'red',
-  },
-  coordinates: {
-    fontSize: 15,
-    color: 'gray',
-  },
-  lastUpdateLabel: {
-<<<<<<< HEAD
-    fontSize: 10,
-    textAlign: 'right',
-=======
-    textAlign: 'left',
->>>>>>> master
-    color: 'gray',
-  },
-  lastUpdate: {
-    textAlign: 'right',
-    fontSize: 20,
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+        height: '88%',
+    },
+    modalContainer: {
+        flex: 1,
+        position: 'absolute',
+        height: '12%',
+        width: '100%',
+        minHeight: 0,
+        backgroundColor: '#fff',
+        zIndex: 1,
+        padding: 20,
+    },
+    modal: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 20,
+        color: 'red',
+    },
+    coordinates: {
+        fontSize: 15,
+        color: 'gray',
+    },
+    lastUpdateLabel: {
+        textAlign: 'left',
+        color: 'gray',
+    },
+    lastUpdate: {
+        textAlign: 'right',
+        fontSize: 20,
+    },
 });
 
 export default App;
--- Register.js
+++ Register.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import { Button, Keyboard, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
 
-
-
 export default class RegistrationScreen extends Component {
-
   fullnameInputRef = React.createRef();
   emailInputRef = React.createRef();
   passwordInputRef = React.createRef();
-  
- 
 
   constructor(props) {
     super(props);
     this.state = {
-        fullname: '',
-        email: '',
-        password: '',
-        showFullnameError: false,
-        showEmailError: false,
-        showPasswordError: false,
+      fullname: '',
+      email: '',
+      password: '',
+      showFullnameError: false,
+      showEmailError: false,
+      showPasswordError: false,
     };
     this.submitPressed = this.submitPressed.bind(this);
   }
@@ -36,175 +31,175 @@ export default class RegistrationScreen extends Component {
     console.log("editNextInput")
     const activeIndex = this.getActiveInputIndex();
     if (activeIndex === -1) {
-        return;
+      return;
     }
 
     const nextIndex = activeIndex + 1;
     if (nextIndex < this.inputs().length && this.inputs()[nextIndex].current != null) {
-        this.setFocus(this.inputs()[nextIndex], true);
+      this.setFocus(this.inputs()[nextIndex], true);
     } else {
-        this.finishEditing();
+      this.finishEditing();
     }
   }
 
   onInputFocus = () => {
     this.setState({
-        activeIndex: this.getActiveInputIndex(),
+      activeIndex: this.getActiveInputIndex(),
     });
   }
 
   onChangeInputHandler = (name, value) => {
     this.setState({
-        [name]: value,
+      [name]: value,
     });
   }
 
   getActiveInputIndex = () => {
     const activeIndex = this.inputs().findIndex((input) => {
-        if (input.current == null) {
-            return false;
-        }
-        
-        return input.current.isFocused();
+      if (input.current == null) {
+        return false;
+      }
+
+      return input.current.isFocused();
     });
-    
+
     return activeIndex;
   }
 
   finishEditing = () => {
     const activeIndex = this.getActiveInputIndex();
     if (activeIndex === -1) {
-        return;
+      return;
     }
     this.setFocus(this.inputs()[activeIndex], false);
   }
 
   setFocus(textInputRef, shouldFocus) {
     if (shouldFocus) {
-        setTimeout(() => {
-            textInputRef.current.focus();
-        }, 100);
+      setTimeout(() => {
+        textInputRef.current.focus();
+      }, 100);
     } else {
-        textInputRef.current.blur();
+      textInputRef.current.blur();
     }
   }
 
   submitPressed() {
     console.log("submitPressed this.state: ", this.state);
     this.setState({
-        showEmailError: this.state.email.length < 4,
-        showPasswordError: this.state.password.length < 4,
-        showFullnameError: this.state.firstname.length < 4,
-        
+      showEmailError: this.state.email.length < 4,
+      showPasswordError: this.state.password.length < 4,
+      showFullnameError: this.state.firstname.length < 4,
+
     });
- 
+
     Keyboard.dismiss();
   }
 
   render() {
     return (
-        <View
-          style={styles.container}
-          contentOffset={{ x: 0, y: 24 }}
-          ref={this._scrollViewRef}
-          scrollEventThrottle={16}
-          contentContainerStyle={{ paddingTop: 24 }}
-          contentInsetAdjustmentBehavior="always"
-          keyboardShouldPersistTaps="handled"
-          keyboardDismissMode="on-drag"
-          enableOnAndroid={true}
-          extraHeight={32}
-          extraScrollHeight={Platform.OS == "android" ? 32 : 0}
-          enableResetScrollToCoords={false}
-          onKeyboardDidShow={this._keyboardDidShowHandler}
-        >
-            <View style={styles.container}>
+      <View
+        style={styles.container}
+        contentOffset={{ x: 0, y: 24 }}
+        ref={this._scrollViewRef}
+        scrollEventThrottle={16}
+        contentContainerStyle={{ paddingTop: 24 }}
+        contentInsetAdjustmentBehavior="always"
+        keyboardShouldPersistTaps="handled"
+        keyboardDismissMode="on-drag"
+        enableOnAndroid={true}
+        extraHeight={32}
+        extraScrollHeight={Platform.OS == "android" ? 32 : 0}
+        enableResetScrollToCoords={false}
+        onKeyboardDidShow={this._keyboardDidShowHandler}
+      >
+        <View style={styles.container}>
 
-                <Text style={styles.header}>Registration</Text>
+          <Text style={styles.header}>Registration</Text>
 
-                <View style={styles.inputTextWrapper}>
-                    <TextInput
-                        placeholder="Email"
-                        style={styles.textInput}
-                        returnKeyType="next"
-                        onSubmitEditing={this.editNextInput}
-                        onFocus={this.onInputFocus}
-                        onChangeText={this.onChangeInputHandler}
-                        ref={this.emailInputRef}
-                    />
-                    {this.state.showEmailError &&
-                        <Text style={styles.errorText}>Please enter your email address.</Text>
-                    }
-                </View>
+          <View style={styles.inputTextWrapper}>
+            <TextInput
+              placeholder="Email"
+              style={styles.textInput}
+              returnKeyType="next"
+              onSubmitEditing={this.editNextInput}
+              onFocus={this.onInputFocus}
+              onChangeText={this.onChangeInputHandler}
+              ref={this.emailInputRef}
+            />
+            {this.state.showEmailError &&
+              <Text style={styles.errorText}>Please enter your email address.</Text>
+            }
+          </View>
 
-                <View style={styles.inputTextWrapper}>
-                    <TextInput
-                        placeholder="Password"
-                        style={styles.textInput}
-                        secureTextEntry={true}
-                        returnKeyType="next"
-                        onSubmitEditing={this.editNextInput}
-                        onFocus={this.onInputFocus}
-                        onChangeText={this.onChangeInputHandler}
-                        ref={this.passwordInputRef}
-                    />
-                    {this.state.showPasswordError &&
-                        <Text style={styles.errorText}>Please enter a password.</Text>
-                    }
-                </View>
+          <View style={styles.inputTextWrapper}>
+            <TextInput
+              placeholder="Password"
+              style={styles.textInput}
+              secureTextEntry={true}
+              returnKeyType="next"
+              onSubmitEditing={this.editNextInput}
+              onFocus={this.onInputFocus}
+              onChangeText={this.onChangeInputHandler}
+              ref={this.passwordInputRef}
+            />
+            {this.state.showPasswordError &&
+              <Text style={styles.errorText}>Please enter a password.</Text>
+            }
+          </View>
 
-                <View style={styles.inputTextWrapper}>
-                    <TextInput
-                        placeholder="Full Name"
-                        style={styles.textInput}
-                        returnKeyType="next"
-                        onSubmitEditing={this.editNextInput}
-                        onFocus={this.onInputFocus}
-                        onChangeText={this.onChangeInputHandler}
-                        ref={this.firstnameInputRef}
-                    />
-                    {this.state.showFirstnameError &&
-                        <Text style={styles.errorText}>Please enter your first name.</Text>
-                    }
-                </View>
+          <View style={styles.inputTextWrapper}>
+            <TextInput
+              placeholder="Full Name"
+              style={styles.textInput}
+              returnKeyType="next"
+              onSubmitEditing={this.editNextInput}
+              onFocus={this.onInputFocus}
+              onChangeText={this.onChangeInputHandler}
+              ref={this.firstnameInputRef}
+            />
+            {this.state.showFirstnameError &&
+              <Text style={styles.errorText}>Please enter your first name.</Text>
+            }
+          </View>
 
-                <View style={styles.btnContainer}>
-                  <Button title="Submit" onPress={this.submitPressed} />
-                </View>
+          <View style={styles.btnContainer}>
+            <Button title="Submit" onPress={this.submitPressed} />
+          </View>
 
-            </View>
         </View>
-      );
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-      paddingBottom: 100,
-    },
-    header: {
-      fontSize: 36,
-      padding: 24,
-      margin: 12,
-      textAlign: "center",
-    },
-    inputTextWrapper: {
-      marginBottom: 24,
-    },
-    textInput: {
-      height: 40,
-      borderColor: "#000000",
-      borderBottomWidth: 1,
-      paddingRight: 30,
-    },
-    errorText: {
-      color: 'red',
-      fontSize: 10,
-    },
-    btnContainer: {
-      backgroundColor: "white",
-      marginTop:36,
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 16,
+    paddingBottom: 100,
+  },
+  header: {
+    fontSize: 36,
+    padding: 24,
+    margin: 12,
+    textAlign: "center",
+  },
+  inputTextWrapper: {
+    marginBottom: 24,
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    paddingRight: 30,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 10,
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 36,
+  }
+});
