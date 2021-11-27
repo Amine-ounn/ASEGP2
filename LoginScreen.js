9c53fe4d85f9287d0a21926fe14046b585d3bffa
Lee Richards <lr337@sussex.ac.uk>
2021-11-27 14:29:36 +0000

    Refactored login form validation

--- LoginScreen.js
+++ LoginScreen.js
@@ -5,20 +5,65 @@ import Theme from './config/Theme';
 
 
 export default function LoginScreen({ navigation }) {
+    const validation = {
+        email: false,
+        password: false
+    }
 
-    const pressHandler = () => { 
+    const checkLogin = () => {
+        // First do validation
+        var errorMsg = "";
+        if (validation.email === false) {
+            errorMsg += "\nInvalid Email Address";
+        }
+        if (validation.password === false) {
+            errorMsg += "\nPassword must contain at least 8 characters and contain upper and lower characters, digits and special characters."
+        }
+        if (errorMsg.length > 0) {
+            alert("ERRORS:" + errorMsg);
+            return false;
+        }
+        return true;
+    }
+    
+    const validateEmail = (text) => {
+        /* Does entered text conform to a valid email address? */
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        reg.test(text) ? validation.email = true : validation.email = false;
+    }
+
+    const validatePassword = (pw) => {
+        /* Password must contain upper, lower, digit, special characters */
+        const reg = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9!@#$%^&*]{8,30}$/;
+        reg.test(pw) ? validation.password = true : validation.password = false;
+    }
+
+    const pressHandler = () => {
         navigation.navigate('Register'); 
       
                                   }
-        const pressHandler2 = () => { 
-        navigation.navigate('Map'); 
+    const pressHandler2 = () => {
+        /* Validate form and then despatch */
+        if (checkLogin()) {
+            navigation.navigate('Map');
+        }
                                     }
     return (
             <KeyboardAvoidingView style={styles.container} behavior="padding">
                 <Image style={styles.logo } source={require("./assets/hot_props_logo.png") } />
                 <View style={styles.controlsContainer}>
-                    <TextInput style={styles.inputControl } placeholder="Email"></TextInput>
-                    <TextInput style={styles.inputControl} placeholder="Password" secureTextEntry></TextInput>
+                <TextInput
+                    style={styles.inputControl}
+                    placeholder="Email"
+                    onChangeText={validateEmail}
+                >
+                </TextInput>
+                <TextInput
+                    style={styles.inputControl}
+                    placeholder="Password" secureTextEntry
+                    onChangeText={validatePassword}
+                >
+                </TextInput>
                     <View style={styles.btnContainer}>
                         <TouchableOpacity onPress={pressHandler2} style={styles.btn}>
                             <Text style={styles.btnText }>Log-in</Text>
